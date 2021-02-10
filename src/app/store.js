ef3830a997eb77646ab7737e71bccbd78535c846
andrmaz <mazbsorz@gmail.com>
2021-02-10 23:27:21 +0100

    Adding redux-logger

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
+import logger from 'redux-logger'
 
 export default configureStore({
     reducer: {
         counter: counterReducer,
     },
+    // - redux-thunk and redux-logger were added as middleware
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    // - The Redux DevTools Extension is disabled for production
+    devTools: process.env.NODE_ENV !== 'production',
 })
